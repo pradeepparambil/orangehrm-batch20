5ae4f9d398b5eeda9dc70dca5e9f97ba217a2b85
Prem Kumar R <premkumarr1989@gmail.com>
2021-06-13 10:37:42 +0530

    employment status - add and edit parameterized operation

--- src/test/java/com/qaguru/orangehrm/pages/EmploymentStatusPage.java
+++ src/test/java/com/qaguru/orangehrm/pages/EmploymentStatusPage.java
@@ -1,9 +1,14 @@
 package com.qaguru.orangehrm.pages;
 
 import com.qaguru.orangehrm.library.PageBase;
+import net.bytebuddy.implementation.bind.annotation.Super;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+
+import java.util.List;
 
 public class EmploymentStatusPage extends PageBase {
     private final String idBtnAdd = "btnAdd";
@@ -13,6 +18,8 @@ public class EmploymentStatusPage extends PageBase {
     private final String idBtnDeleteConform = "dialogDeleteBtn";
     private final String xpNewStatus = "//a[text()='XXX']";
     private final String xpChkEmpDelete = "//a[text()='XXX']//preceding :: input[@name='chkSelectRow[]'][1]";
+    private final String xpEmpEdit = "//a[contains(text(),'XXX')]";
+    private final String idEditTxtStatus = "empStatus_name";
 
 
     public EmploymentStatusPage(WebDriver driver) {
@@ -29,11 +36,23 @@ public class EmploymentStatusPage extends PageBase {
     }
 
     public void deleteNewEmpStatus(String status) {
-        super.click(By.xpath(xpChkEmpDelete.replace("XXX",status)));
+        super.click(By.xpath(xpChkEmpDelete.replace("XXX", status)));
         super.click(By.id(idBtnDelete));
         super.click(By.id(idBtnDeleteConform));
         Assert.assertFalse(super.isElementVisible(By.xpath(xpNewStatus.replace("XXX", status))),
                 "The new status is not deleted");
 
     }
+
+    public void editEmpStatus(String stat, String status) {
+        super.click(By.xpath(xpEmpEdit.replace("XXX", stat)));
+        super.setText(By.id(idEditTxtStatus), status);
+        super.click(By.id(idBtnSave));
+    }
+
 }
+
+
+
+
+
--- src/test/java/com/qaguru/orangehrm/tests/EmploymentStatusTest.java
+++ src/test/java/com/qaguru/orangehrm/tests/EmploymentStatusTest.java
@@ -1,12 +1,17 @@
 package com.qaguru.orangehrm.tests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qaguru.orangehrm.library.TestBase;
+import com.qaguru.orangehrm.models.EmpStatus;
 import com.qaguru.orangehrm.pages.EmploymentStatusPage;
 import com.qaguru.orangehrm.pages.HeaderPage;
 import com.qaguru.orangehrm.pages.LoginPage;
 import com.qaguru.orangehrm.pages.MenuOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.UUID;
 
 public class EmploymentStatusTest extends TestBase {
@@ -42,4 +47,24 @@ public class EmploymentStatusTest extends TestBase {
     }
 
 
+    @Test(dataProvider = "addEmpStatus", dataProviderClass = EmploymentStatusTestData.class)
+    public void editEmpStatusParameterized(String statusFile) throws IOException {
+        URL url = getClass().getClassLoader().getResource(statusFile);
+        ObjectMapper mapper = new ObjectMapper();
+        EmpStatus status = mapper.readValue(url, EmpStatus.class);
+
+        LoginPage loginpage = new LoginPage(driver);
+        loginpage.login(status.getUserName(), status.getPassword());
+
+        HeaderPage headerPage = new HeaderPage(driver);
+        headerPage.selectMenu(MenuOptions.EMPLOYMENT_STATUS);
+
+        EmploymentStatusPage employmentStatusPage = new EmploymentStatusPage(driver);
+        for (String stat : status.getStatus()) {
+            String statusname = stat + UUID.randomUUID();
+            employmentStatusPage.addNewEmpStatus(statusname);
+            employmentStatusPage.editEmpStatus(statusname, statusname.split("-")[0] + " edited format");
+
+        }
+    }
 }
